241ccc14695f4eb2f3af61d5d63bc4d585f1a9a5
younis <yereiqat@gmail.com>
2024-07-26 23:43:27 -0700

    launch screen added

--- iOSFetchExercise/iOSFetchExercise/iOSFetchExerciseApp.swift
+++ iOSFetchExercise/iOSFetchExercise/iOSFetchExerciseApp.swift
@@ -9,9 +9,35 @@ import SwiftUI
 
 @main
 struct iOSFetchExerciseApp: App {
+    @State private var isActive = false
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            if isActive {
+                ContentView()
+            } else {
+                LaunchScreenView()
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                            withAnimation {
+                                self.isActive = true
+                            }
+                        }
+                    }
+            }
+        }
+    }
+}
+
+struct LaunchScreenView: View {
+    var body: some View {
+        GeometryReader { geometry in
+            Image("LaunchScreenImage")
+                .resizable()
+                .scaledToFit()
+                .frame(width: geometry.size.width, height: geometry.size.height)
+                .background(Color.black) // Add a background color if needed
+                .edgesIgnoringSafeArea(.all)
         }
     }
 }
